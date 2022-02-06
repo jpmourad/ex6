9bc180fd57305aa7e4cecc1911a0fb5cae6f8a7b
João Paulo <joao_mourad@live.com>
2022-02-06 04:53:03 -0300

    Initial Commit: Basic Structure and simple shape classes. Tests don't pass.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,27 @@
 import atributos.Bovino;
 import atributos.Raca;
+import connection.Insert_Value;
+import connection.Read_Value;
 
 
 public class Main {
     public static void main(String[] args) {
-        Bovino bovino = new Bovino("brinco", "Angelino", "Morto", "", "", "001",
+        Bovino bovino = new Bovino("brinco", "Angelino", "Morto", "F", "", "001",
                 Raca.RACA_GIR,"01/02/2018", "", "", "");
         System.out.println(bovino);
+
+       Insert_Value inserir = new Insert_Value(bovino);
+       inserir.insert();
+
+        Read_Value ler = new Read_Value();
+        ler.read(bovino.getBrinco(),null);
+        ler.read(null, bovino.getNome());
     }
 
 
 }
+
+
+
+
+
